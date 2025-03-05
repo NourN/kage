7705c3b217647c0b544eb7fa0bcde88ad0154125
NourDev <nour@skillspotting.com>
2025-03-05 13:01:21 +0100

    fix time

--- script.js
+++ script.js
@@ -1,39 +1,45 @@
-document.addEventListener("DOMContentLoaded", function() {
-  function updateTime(city, timezone) {
-      const timeElement = document.querySelector(`#${city} .time`);
-      const url = `http://worldtimeapi.org/api/timezone/${timezone}.json`;
-      fetch(url)
-          .then(response => response.json())
-          .then(data => {
-              const time = new Date(data.utc_datetime);
-              timeElement.innerText = time.toLocaleTimeString();
-          });
-  }
+const apiKey = "c9a24fbb413edfeefaafe17df260aa25";
+  
+function updateTime(city, timezone) {
+  const timeElement = document.querySelector(`#${city} .time`);
+  const time = new Date().toLocaleTimeString('en-US', { timeZone: `${timezone}`, timeZoneName: 'short' });
 
-  function updateWeather(city, apiKey) {
-      const weatherElement = document.querySelector(`#${city} .weather`);
-      const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
-      fetch(url)
-          .then(response => response.json())
-          .then(data => {
-              const temp = data.main.temp;
-              weatherElement.innerText = `${temp}°C`;
-          });
-  }
+  const [hour, minute, second] = time.split(':');
+  const formattedTime = `${hour.padStart(2, '0')}:${minute.padStart(2, '0')}:${second.padStart(2, '0')}`;
+  
+  
+  timeElement.innerText = formattedTime;
+        
+}
 
-  const apiKey = "22f28d4e6aa7d08955d7827877b3fede";
+/* function updateWeather(city, apiKey) {
+  const weatherElement = document.querySelector(`#${city} .weather`);
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+  
+  fetch(url)
+    .then(response => response.json())  // Il faut utiliser .json() ici
+    .then(data => {
+        const temp = data.main.temp;
+        console.log(temp);
+        weatherElement.innerText = `${temp}°C`;
+    })
+    .catch(error => {
+        console.error("Error fetching weather data:", error);
+        weatherElement.innerText = "Failed to load weather data";
+    });
+}
 
-  updateTime("tokyo", "Asia/Tokyo");
-  updateTime("losangeles", "America/Los_Angeles");
-  updateTime("newyork", "America/New_York");
 
-  updateWeather("Tokyo", apiKey);
-  updateWeather("Los Angeles", apiKey);
-  updateWeather("New York", apiKey);
+updateWeather("Tokyo");
+updateWeather("Los Angeles");
+updateWeather("New York"); */
 
-  setInterval(() => {
-      updateTime("tokyo", "Asia/Tokyo");
-      updateTime("losangeles", "America/Los_Angeles");
-      updateTime("newyork", "America/New_York");
-  }, 1000);
-});
+setInterval(() => {
+    updateTime("tokyo", "Asia/Tokyo");
+    updateTime("losangeles", "America/Los_Angeles");
+    updateTime("newyork", "America/New_York");
+}, 1000);
+
+
+//[35.6895, 139.6917] tokyo
+
