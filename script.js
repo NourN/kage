bd5037e6d3e81c4052d26b7333e38fc664dde77a
NourDev <nour@skillspotting.com>
2025-03-05 09:24:00 +0100

    some modifications

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const h1 = document.querySelector("h1");
+  setTimeout(() => {
+      h1.style.color = "#ff6347"; // Change color after 2 seconds
+  }, 2000);
+});
